f8d04d92951718cbc9358712d0b2a3c724eeae29
clare <clare_baek@naver.com>
2021-04-25 16:12:02 +0900

    샘플씬설정

--- PROJECT_T/Assets/2. Script -------------------------/Ingame/IngameScene.cs
+++ PROJECT_T/Assets/2. Script -------------------------/Ingame/IngameScene.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections;
+﻿using BehaviorDesigner.Runtime.Tasks.Unity.UnityGameObject;
+using Doozy.Engine.UI;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IngameScene : MonoBehaviour
 {
+    public List<UIButton> buttonList = new List<UIButton>();
+    public GameObject buttonPOP;
+
+    private void Awake()
+    {
+    }
+
+    private void OnEnable()
+    {
+        InitButtonList();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,26 @@ public class IngameScene : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void InitButtonList()
+    {
+        buttonPOP = GameObject.FindWithTag("UIPopup");
+        buttonPOP.SetActive(false);
+
+        var temp = GameObject.FindGameObjectsWithTag("UIButton");
+
+        foreach(var tempGO in temp)
+        {
+            buttonList.Add(tempGO.GetComponent<UIButton>());
+        }
+
+        foreach(var button in buttonList)
+        {
+            button.OnClick.OnTrigger.Event.AddListener(() =>
+            {
+                buttonPOP.SetActive(true);
+            });
+        }
     }
 }
